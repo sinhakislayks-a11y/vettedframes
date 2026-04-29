101da42ea33c343c222683632ea49e60bd62e0e6
Kislay Sinha <kislaysinha@Kislays-Mac-mini.local>
2026-04-29 17:06:44 +0530

    fix: sitemap URL, move to correct location

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,17 +3,20 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://frames.kislay.in",
+      url: "https://project-nd6p6.vercel.app",
+      lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1.0,
     },
     {
-      url: "https://frames.kislay.in/workflow",
+      url: "https://project-nd6p6.vercel.app/workflow",
+      lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.7,
     },
     {
-      url: "https://frames.kislay.in/contact",
+      url: "https://project-nd6p6.vercel.app/contact",
+      lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.7,
     },
