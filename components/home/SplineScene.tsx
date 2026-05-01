66eb6faf8c5f03fa40b56ecf58ec8337a44c1680
Kislay Sinha <kislaysinha@Kislays-Mac-mini.local>
2026-05-01 06:09:13 +0530

    fix: bump spline cache-bust version to load latest changes

--- components/home/SplineScene.tsx
+++ components/home/SplineScene.tsx
@@ -5,7 +5,7 @@ import { useEffect, useRef, useState } from "react";
 // Spline viewer URL (iframe embed method)
 // Cache-bust: update this timestamp whenever you re-export from the Spline editor
 const SPLINE_SCENE_URL =
-  "https://my.spline.design/theeternalarc-tkcFHBzOasiJym6BQGBfeSpd-xWa/?v=20260501";
+  "https://my.spline.design/theeternalarc-tkcFHBzOasiJym6BQGBfeSpd-xWa/?v=20260501b";
 
 function GradientFallback() {
   return (
